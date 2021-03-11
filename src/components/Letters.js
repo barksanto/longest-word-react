7d577d4fc48ccaf855664dab7d723d57956615fb
Barkley Santo <barkley.santo@gmail.com>
2021-03-11 12:14:17 -0500

    added APi url

--- src/components/Letters.js
+++ src/components/Letters.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+const app_id = '';
+const app_key = ''
+let url = "https://od-api.oxforddictionaries.com/api/v2/<endpoint>/<language_code>/<word_id>"
+
 const Letters = () => {
   return (
     <div>
