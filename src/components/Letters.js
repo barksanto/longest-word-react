af809a47b21d256bf6919051b189742f42dffb5e
Barkley Santo <barkley.santo@gmail.com>
2021-03-11 12:22:51 -0500

    added app id and app key

--- src/components/Letters.js
+++ src/components/Letters.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
-const app_id = '';
-const app_key = ''
-let url = "https://od-api.oxforddictionaries.com/api/v2/<endpoint>/<language_code>/<word_id>"
+// baseurl
+// appid
+// appkey
+
+const app_id = 'a15916a7';
+const app_key = 'fb6b989ea04203c3c31d917404241125'
+let endpoint = "entries"
+let language_code = "en-us"
+let word_id = "example"
+let url = `https://od-api.oxforddictionaries.com/api/v2/${endpoint}/${language_code}/${word_id.toLowerCase()}`
 
 const Letters = () => {
   return (
