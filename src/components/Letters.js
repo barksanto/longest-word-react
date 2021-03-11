e77653a37c07612dd78f36f94c3ba972df21729a
Barkley Santo <barkley.santo@gmail.com>
2021-03-11 12:53:55 -0500

    changed from Oxford Api to Rapid API

--- src/components/Letters.js
+++ src/components/Letters.js
@@ -3,18 +3,27 @@ import React from 'react';
 // baseurl
 // appid
 // appkey
+let word = 'dog'
 
-const app_id = 'a15916a7';
-const app_key = 'fb6b989ea04203c3c31d917404241125'
-let endpoint = "entries"
-let language_code = "en-us"
-let word_id = "example"
-let url = `https://od-api.oxforddictionaries.com/api/v2/${endpoint}/${language_code}/${word_id.toLowerCase()}`
+fetch(`https://wordsapiv1.p.rapidapi.com/words/${word}`, {
+  "method": "GET",
+  "headers": {
+    "x-rapidapi-key": "c069a2cdecmshf6c6185371da6d1p100505jsn0552157e6778",
+    "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
+  }
+})
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.error(err);
+  });
 
 const Letters = () => {
   return (
     <div>
       <div>I'm the Letters Component</div>
+      <input type="text" spellCheck="true" />
     </div>
   );
 }
