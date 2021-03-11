9e3a8c82c010e0b446cf189d44023329b3de7a56
Barkley Santo <barkley.santo@gmail.com>
2021-03-10 19:34:10 -0500

    import Letters component into App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Letters from './components/Letters'
 
 function App() {
   return (
     <div className="App">
       <div>I'm the App component</div>
       <h1>Welcome to the longest word game!</h1>
-
+      <Letters />
     </div>
   );
 }
