d10df4b600a9cfac8a309c070de70ee941804a06
Barkley Santo <barkley.santo@gmail.com>
2021-03-12 11:07:51 -0500

    fetchQPi on user submit click

--- src/components/Letters.js
+++ src/components/Letters.js
@@ -11,25 +11,26 @@ const Letters = () => {
 
   let word = 'dog';
 
-  fetch(`https://wordsapiv1.p.rapidapi.com/words/${word}`, {
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-key": "c069a2cdecmshf6c6185371da6d1p100505jsn0552157e6778",
-      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
-    }
-  })
-    .then(response => {
-      console.log(response);
-    })
-    .catch(err => {
-      console.error(err);
-    });
-
-  let inputGuess = document.getElementById('fname').value
+  const inputGuess = document.getElementById('fname');
 
   const captureGuess = (e) => {
     e.preventDefault();
-    console.log(inputGuess);
+    word = inputGuess.value;
+
+    fetch(`https://wordsapiv1.p.rapidapi.com/words/${word}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "c069a2cdecmshf6c6185371da6d1p100505jsn0552157e6778",
+        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
+      }
+    })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    console.log(word)
   }
 
   return (
