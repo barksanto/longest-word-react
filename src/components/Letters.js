4e69d007ec6cc794c9d1029cae07ce436e94c181
Barkley Santo <barkley.santo@gmail.com>
2021-03-12 11:00:50 -0500

    capture input form user on submit

--- src/components/Letters.js
+++ src/components/Letters.js
@@ -1,19 +1,47 @@
 import React, { setState, useState } from 'react';
 require('dotenv').config();
-
+console.log(process.env)
 
 // baseurl
 // appid
 // appkey
-let word = 'dog'
-
 
 
 const Letters = () => {
+
+  let word = 'dog';
+
+  fetch(`https://wordsapiv1.p.rapidapi.com/words/${word}`, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "c069a2cdecmshf6c6185371da6d1p100505jsn0552157e6778",
+      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
+    }
+  })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+
+  let inputGuess = document.getElementById('fname').value
+
+  const captureGuess = (e) => {
+    e.preventDefault();
+    console.log(inputGuess);
+  }
+
   return (
     <div>
       <div>I'm the Letters Component</div>
-      <input type="text" spellCheck="true" />
+
+      <form id="nameform">
+        <label for="fname">Your guess</label>
+        <input type="text" id="fname" name="fname" spellCheck="true" />
+      </form>
+      <button type="submit" form="nameform" value="Submit" onClick={captureGuess}>Submit</button>
+
     </div>
   );
 }
