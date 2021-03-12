d19583da7512ec8efdf0898d2599766e74c13514
Barkley Santo <barkley.santo@gmail.com>
2021-03-12 11:57:49 -0500

    create array of 8 random letters

--- src/components/RandomLetters.js
+++ src/components/RandomLetters.js
@@ -3,11 +3,24 @@ import React from 'react';
 const RandomLetters = () => {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
   // console.log(alphabet[Math.floor(Math.random() * alphabet.length)])
-  console.log(alphabet[Math.floor(Math.random() * alphabet.length)]);
+
+
+  // get 8 random letters
+  const randomLetters = [];
+
+  let i;
+  for (i = 0; i < 8; i++) {
+    console.log(alphabet[Math.floor(Math.random() * alphabet.length)]);
+    randomLetters.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
+  }
+  console.log(randomLetters)
 
   return (
     <div>
       random letters component
+      {
+
+      }
     </div>
   );
 }
