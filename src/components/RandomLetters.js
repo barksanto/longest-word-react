beb67b0392a80f236f87c0e739404e64a6acf5a5
Barkley Santo <barkley.santo@gmail.com>
2021-03-12 12:00:03 -0500

    render 8 rendom letters in their own fiv

--- src/components/RandomLetters.js
+++ src/components/RandomLetters.js
@@ -19,7 +19,9 @@ const RandomLetters = () => {
     <div>
       random letters component
       {
-
+        randomLetters.map(letter => {
+          return (<div>{letter}</div>)
+        })
       }
     </div>
   );
