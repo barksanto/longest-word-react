d6e1962dfb5ccb9fb5c867ae0c2c19dae05c53f2
Barkley Santo <barkley.santo@gmail.com>
2021-03-16 11:56:03 -0500

    refactor for loop for user input

--- src/App.js
+++ src/App.js
@@ -3,29 +3,58 @@ import './App.css';
 import InputBox from './components/InputBox';
 import RandomLetters from './components/RandomLetters';
 
-
 function App() {
 
   const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  // get 8 random letters - pass them down through props to funcitonal componentsm
+  // get 8 random letters - pass them down through props to funcitonal components
+  // initialize array
   const randomLetters = [];
-  let i;
-  for (i = 0; i < 8; i++) {
+  // 
+  
+  for (let i = 0; i < 8; i++) {
     console.log(alphabet[Math.floor(Math.random() * alphabet.length)]);
     randomLetters.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
   }
 
+  
 
-  const handleCallback = (childData) => {
-    
+// code to capture user input in submit box
+  let word = '';
+
+  const inputGuess = document.querySelector('.userInput');
+
+  const captureGuess = (e) => {
+    e.preventDefault();
+    word = inputGuess.value;
+
+    fetch(`https://wordsapiv1.p.rapidapi.com/words/${word}`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "c069a2cdecmshf6c6185371da6d1p100505jsn0552157e6778",
+        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
+      }
+    })
+      .then(response => {
+        console.log(response.status);
+
+        if (response.status === 200) {
+          alert("This word exists!")
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      }).then(()=>{
+        // inputGuess.value = '';
+      })
   }
 
+  
   return (
     <div className="App">
       <div>I'm the App component</div>
       <h1>Welcome to the longest word game!</h1>
       <RandomLetters letterData={randomLetters} />
-      <InputBox />
+      <InputBox captureGuess={captureGuess}/>
     </div>
   );
 }
--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -3,37 +3,9 @@ import React from 'react';
 require('dotenv').config();
 // console.log(process.env)
 
-const InputBox = () => {
+const InputBox = (props) => {
 
-  let word = '';
 
-  const inputGuess = document.querySelector('.userInput');
-
-  const captureGuess = (e) => {
-    e.preventDefault();
-    word = inputGuess.value;
-    // let userGuess = word;
-
-    fetch(`https://wordsapiv1.p.rapidapi.com/words/${word}`, {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-key": "c069a2cdecmshf6c6185371da6d1p100505jsn0552157e6778",
-        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
-      }
-    })
-      .then(response => {
-        console.log(response.status);
-
-        if (response.status === 200) {
-          alert("This word exists!")
-        }
-      })
-      .catch(err => {
-        console.error(err);
-      }).then(()=>{
-        inputGuess.value = '';
-      })
-  }
 
   return (
     <div className="mt-3">
@@ -41,7 +13,7 @@ const InputBox = () => {
         <label htmlFor="fname">Your guess: </label>
         <input type="text" id="fname" name="fname" className="userInput" spellCheck="true" />
       </form>
-      <button type="submit" form="nameform" value="Submit" onClick={captureGuess}>Submit</button>
+      <button type="submit" form="nameform" value="Submit" onClick={props.captureGuess}>Submit</button>
     </div>
   );
 }
