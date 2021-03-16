8a8b9d5049adb0cd13a8f46d1cc3528c437ed85d
Barkley Santo <barkley.santo@gmail.com>
2021-03-16 11:17:54 -0500

    mopved random letter logic to App level component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,21 @@ import RandomLetters from './components/RandomLetters';
 
 
 function App() {
+
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  // get 8 random letters
+  const randomLetters = [];
+  let i;
+  for (i = 0; i < 8; i++) {
+    console.log(alphabet[Math.floor(Math.random() * alphabet.length)]);
+    randomLetters.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
+  }
+
   return (
     <div className="App">
       <div>I'm the App component</div>
       <h1>Welcome to the longest word game!</h1>
-      <RandomLetters />
+      <RandomLetters letterData={randomLetters} />
       <InputBox />
     </div>
   );
--- src/components/RandomLetters.js
+++ src/components/RandomLetters.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import '../styles/random-letter.css'
 
-const RandomLetters = () => {
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  // get 8 random letters
-  const randomLetters = [];
-  let i;
-  for (i = 0; i < 8; i++) {
-    console.log(alphabet[Math.floor(Math.random() * alphabet.length)]);
-    randomLetters.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
-  }
+const RandomLetters = ({ letterData }) => {
+
 
   return (
     <div className="container">
       Random Letters Component
       <div className="row justify-content-between">
         {
-          randomLetters.map(letter => {
+          letterData.map(letter => {
             return (<div className="col-1 letter" key={Math.random()}>{letter}</div>)
           })
         }
