a4ca541e8ad8f6feaef60e5a1dd057ca0c3df021
Barkley Santo <barkley.santo@gmail.com>
2021-03-16 11:48:02 -0500

    added callback function to get data fro inputbox component value

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react'
 import './App.css';
 import InputBox from './components/InputBox';
 import RandomLetters from './components/RandomLetters';
@@ -6,12 +7,17 @@ import RandomLetters from './components/RandomLetters';
 function App() {
 
   const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  // get 8 random letters
+  // get 8 random letters - pass them down through props to funcitonal componentsm
   const randomLetters = [];
   let i;
   for (i = 0; i < 8; i++) {
     console.log(alphabet[Math.floor(Math.random() * alphabet.length)]);
     randomLetters.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
+  }
+
+
+  const handleCallback = (childData) => {
+    
   }
 
   return (
