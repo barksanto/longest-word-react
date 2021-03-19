7fc07596901f8f223ee83ce8e364722c760349d4
Barkley Santo <barkley.santo@gmail.com>
2021-03-19 16:49:44 -0600

    larged submit button 150px

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -8,7 +8,7 @@ const InputBox = (props) => {
   return (
     <div className="mt-3 container">
       <form id="nameform" className="row justify-content-center">
-        <label htmlFor="fname" >Your guess: </label><br />
+        <label htmlFor="fname" className="">Your guess: </label><br />
         <input type="text" id="fname" name="fname" className="userInput" spellCheck="true" />
       </form>
       <button className="submit-button row justify-content-center" type="submit" form="nameform" value="Submit" onClick={props.captureGuess}>Submit</button>
