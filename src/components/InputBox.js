ee80619cd04b504797f8555677b05b7dceb8a311
Barkley Santo <barkley.santo@gmail.com>
2021-03-20 15:58:26 -0600

    moved label outside of div for even spacing

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -7,8 +7,8 @@ const InputBox = (props) => {
 
   return (
     <div className="mt-3 container">
+      <label htmlFor="fname" className="">Your guess: </label>
       <form id="nameform" className="row justify-content-center">
-        <label htmlFor="fname" className="">Your guess: </label><br />
         <input type="text" id="fname" name="fname" className="userInput" spellCheck="true" />
       </form>
       <button className="submit-button row justify-content-center" type="submit" form="nameform" value="Submit" onClick={props.captureGuess}>Submit</button>
